233cb88e31b1d21e2fa1e2a39657d39d1ede212e
naga pranav <nagapranav.3128t@gmail.com>
2023-08-16 13:27:54 +0530

    commited the tictac board with x

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,22 +1,47 @@
+import { useState } from 'react';
 import Square from './square';
 
 const Board = () => {
+  const [squares, setsquares] = useState(Array(9).fill(null));
+
+  console.log(squares);
+
+  const handlesquareclick = clickedposition => {
+    setsquares(currentsquares =>{
+      return currentsquares.map((squarevalue,position)=>{
+        if(clickedposition == position){
+        return 'X'
+      }
+
+      return squarevalue;
+    });
+    });
+  };
+
+  const render = position => {
+    return (
+      <Square
+        value={squares[position]}
+        onClick={() => handlesquareclick(position)}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {render(0)}
+        {render(1)}
+        {render(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {render(3)}
+        {render(4)}
+        {render(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {render(6)}
+        {render(7)}
+        {render(8)}
       </div>
     </div>
   );
--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,6 +1,10 @@
-const square = ({ value }) => {
+const square = ({ value, onClick }) => {
   //props can be directly used and use props.value to get value also can use this method
-  return <button type="button" className="square">{value}</button>;
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 export default square;
